5abb18f83127634d0c5665cb998adb2098d55abf
Junior Dering <juniordering@hotmail.com>
2024-04-28 14:23:20 -0400

    initial commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { theme } from '@/theme'
+import { View, StyleSheet, Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.black,
+  },
+  text: {
+    fontSize: 22,
+    color: theme.colors.white
+  },
+})
---
+++ src/theme/color.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  white: '#fff',
+  black: '#000',
+  gray:{
+    600: '#8e8e8e',
+    700: '#4b4b4b',
+    800: '#292929',
+  }
+}
